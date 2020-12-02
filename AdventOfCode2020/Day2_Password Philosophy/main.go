8b5c4723e88af4743370be2c5ec97a104340f341
Stanislav <stani.petrosyan@gmail.com>
2020-12-02 20:38:16 +0100

    solved day 2

---
+++ AdventOfCode2020/Day2_Password Philosophy/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"adventofcode/util"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Password struct {
+	policy []string
+	char   byte
+	value  string
+}
+
+func main() {
+	lines, err := util.ReadLines("./input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	solvePartOne(lines)
+	solvePartTwo(lines)
+}
+
+func solvePartOne(lines []string) {
+	var solution = 0
+	for _, line := range lines {
+		password := serializeStringToPassword(line)
+		count := strings.Count(password.value, string(password.char))
+		min, _ := strconv.Atoi(password.policy[0])
+		max, _ := strconv.Atoi(password.policy[1])
+		if count >= min && count <= max {
+			solution++
+		}
+	}
+	println(solution)
+}
+
+func solvePartTwo(lines []string) {
+	var solution = 0
+	for _, line := range lines {
+		password := serializeStringToPassword(line)
+		start, _ := strconv.Atoi(password.policy[0])
+		end, _ := strconv.Atoi(password.policy[1])
+		if (password.value[start] == password.char) != (password.value[end] == password.char) {
+			solution++
+		}
+	}
+	println(solution)
+}
+
+func serializeStringToPassword(line string) Password {
+	var password = Password{}
+	line = strings.TrimSpace(line)
+	var split = strings.Split(line, ":")
+	var first = split[0]
+	password.value = split[1]
+	password.char = first[len(first)-1]
+	password.policy = strings.Split(first[:len(first)-2], "-")
+	return password
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# AdventOfCode 2020
+
+## Day 1: Report Repair
+
+## Day 2: Password Philosophy
