c89d313ad28c609b306ed95fda063979f237554f
Stanislav <stani.petrosyan@gmail.com>
2020-12-17 21:11:38 +0100

    solved day 8

--- AdventOfCode2020/Day8_Handheld Halting/main.go
+++ AdventOfCode2020/Day8_Handheld Halting/main.go
@@ -28,25 +28,63 @@ func main() {
 	}
 
 	solvePartOne(instructions)
-
+	solvePartTwo(instructions)
 }
 
 func solvePartOne(instructions []Instruction) {
+	tmp := make([]Instruction, len(instructions))
+	copy(tmp, instructions)
 	var accumulator int = 0
 
-	for index := 0; instructions[index].exec == false; {
-		instructions[index].exec = true
-		arg := instructions[index].arg
-
-		switch instructions[index].op {
-		case "acc":
-			accumulator += arg
-			index++
-		case "jmp":
-			index += arg
-		case "nop":
-			index++
-		}
+	for index := 0; tmp[index].exec == false; {
+		tmp[index].exec = true
+		accumulator, index = tmp[index].execInstruction(accumulator, index)
 	}
 	println(accumulator)
+}
+
+func solvePartTwo(instructions []Instruction) {
+
+	for index, item := range instructions {
+		channel := make(chan int)
+		tmp := make([]Instruction, len(instructions))
+		copy(tmp, instructions)
+		tmp[index].exec = false
+		if item.op == "jmp" {
+			tmp[index].op = "nop"
+		} else if item.op == "nop" {
+			tmp[index].op = "jmp"
+		}
+		go runHeldhandGame(tmp, channel)
+		solution := <-channel
+		if solution != 0 {
+			println(solution)
+		}
+	}
+}
+
+func runHeldhandGame(instructions []Instruction, ch chan<- int) {
+	var accumulator int = 0
+	for index := 0; instructions[index].exec == false; {
+		instructions[index].exec = true
+		accumulator, index = instructions[index].execInstruction(accumulator, index)
+		if index == len(instructions) {
+			ch <- accumulator
+			break
+		}
+	}
+	ch <- 0
+}
+
+func (i *Instruction) execInstruction(accumulator, index int) (int, int) {
+	switch i.op {
+	case "acc":
+		accumulator += i.arg
+		index++
+	case "jmp":
+		index += i.arg
+	case "nop":
+		index++
+	}
+	return accumulator, index
 }
