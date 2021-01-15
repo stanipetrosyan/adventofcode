8c9e2ad0ca940d191b6479986ede38123fc3f120
Stanislav <stani.petrosyan@gmail.com>
2021-01-15 20:37:08 +0100

    solved day 12

--- AdventOfCode2020/Day12_Rain Risk/main.go
+++ AdventOfCode2020/Day12_Rain Risk/main.go
@@ -26,46 +26,71 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var position Position = Position{0, 0, "E"}
 
+	solvePartOne(lines)
+	solvePartTwo(lines)
+}
+
+func solvePartOne(lines []string) {
+	var ship Position = Position{0, 0, "E"}
 	for _, line := range lines {
 		value, _ := strconv.Atoi(line[1:])
 		action := Action{string(line[0]), value}
 		if action.name == "R" || action.name == "L" {
-			position.setOrientation(action)
+			ship.orientation = moveOrientation(action, ship.orientation)
 		} else {
 			if action.name == "F" {
-				position.movePosition(position.orientation, value)
+				ship.movePosition(ship.orientation, value)
 			} else {
-				position.movePosition(action.name, value)
+				ship.movePosition(action.name, value)
 			}
 		}
 	}
 
-	println(manhattanDistance(position))
+	println(manhattanDistance(ship))
+}
+
+func solvePartTwo(lines []string) {
+	var waypoint Position = Position{10, 1, "E"}
+	var ship Position = Position{0, 0, "E"}
+	for _, line := range lines {
+		value, _ := strconv.Atoi(line[1:])
+		action := Action{string(line[0]), value}
+		if action.name == "R" || action.name == "L" {
+			waypoint.rotate(action)
+		} else {
+			if action.name == "F" {
+				orientation := waypointOrientation(waypoint)
+				ship.movePosition(orientation[0], int(math.Abs(float64(waypoint.x*value))))
+				ship.movePosition(orientation[1], int(math.Abs(float64(waypoint.y*value))))
+			} else {
+				waypoint.movePosition(action.name, value)
+			}
+		}
+	}
+
+	println(manhattanDistance(ship))
 }
 
 func (p *Position) movePosition(name string, value int) {
 	switch name {
 	case "N":
-		p.x += value
-	case "S":
-		p.x -= value
-	case "W":
-		p.y -= value
-	case "E":
 		p.y += value
+	case "S":
+		p.y -= value
+	case "W":
+		p.x -= value
+	case "E":
+		p.x += value
 	}
 }
 
-func (p *Position) setOrientation(action Action) {
+func moveOrientation(action Action, precOrientation string) string {
 	var move int = action.value / 90
 	if action.name == "L" {
 		move = (move * -1) + len(coordinates)
 	}
-	var position int = (findCoordinate(p.orientation) + move) % len(coordinates)
-
-	p.orientation = coordinates[position]
+	return coordinates[(findCoordinate(precOrientation)+move)%len(coordinates)]
 }
 
 func findCoordinate(name string) int {
@@ -78,6 +103,81 @@ func findCoordinate(name string) int {
 	return -1
 }
 
-func manhattanDistance(position Position) int {
-	return int(math.Abs(float64(position.x)) + math.Abs(float64(position.y)))
+func (p *Position) rotate(action Action) {
+	tmpPosition := Position{0, 0, "E"}
+
+	for _, item := range waypointOrientation(*p) {
+		dimensioneRotation := moveOrientation(action, item)
+		tmpPosition.setOrientation(*p, item, dimensioneRotation)
+	}
+
+	p.x = tmpPosition.x
+	p.y = tmpPosition.y
+}
+
+func (tmpPosition *Position) setOrientation(p Position, orientation string, dimensioneRotation string) {
+	if orientation == "E" {
+		switch dimensioneRotation {
+		case "N":
+			tmpPosition.y = p.x
+		case "W":
+			tmpPosition.x = -p.x
+		case "S":
+			tmpPosition.y = -p.x
+		}
+	}
+
+	if orientation == "N" {
+		switch dimensioneRotation {
+		case "E":
+			tmpPosition.x = p.y
+		case "W":
+			tmpPosition.x = -p.y
+		case "S":
+			tmpPosition.y = -p.y
+		}
+	}
+
+	if orientation == "S" {
+		switch dimensioneRotation {
+		case "E":
+			tmpPosition.x = -p.y
+		case "W":
+			tmpPosition.x = p.y
+		case "N":
+			tmpPosition.y = -p.y
+		}
+	}
+
+	if orientation == "W" {
+		switch dimensioneRotation {
+		case "E":
+			tmpPosition.x = -p.x
+		case "N":
+			tmpPosition.y = -p.x
+		case "S":
+			tmpPosition.y = p.x
+		}
+	}
+}
+
+func waypointOrientation(p Position) []string {
+	var orientation []string = []string{"", ""}
+	if p.x > 0 {
+		orientation[0] = "E"
+	} else {
+		orientation[0] = "W"
+	}
+
+	if p.y > 0 {
+		orientation[1] = "N"
+	} else {
+		orientation[1] = "S"
+	}
+
+	return orientation
+}
+
+func manhattanDistance(ship Position) int {
+	return int(math.Abs(float64(ship.x)) + math.Abs(float64(ship.y)))
 }
